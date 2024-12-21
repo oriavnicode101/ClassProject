abc89a98aeb6d32b98aa111c7bd17d4f576f0fa5
orinmed25 <orin20002510@gmail.com>
2024-12-21 13:20:53 +0200

    Delete main.cpp

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
